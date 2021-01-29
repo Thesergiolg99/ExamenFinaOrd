d438e9cd1a21465e31ab026e18a2c49b61a5dfa5
Sergio <sergiol199937@gmail.com>
2021-01-29 18:50:08 +0100

    Creacion y saptacion del proyecto

---
+++ src/main/java/ufv/dis/final2021/SLG/LeerJson.java
@@ -0,0 +1,5 @@
+package ufv.dis.final2021.SLG;
+
+public class LeerJson {
+
+}
---
+++ src/test/java/ufv/dis/final2020/SLG/SlgApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2020.SLG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
